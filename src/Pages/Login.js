7ad47ab19de3e335c179803e52d8f0b793db5989
Ana Carolina Magalhaes <carol2015bortolini@gmail.com>
2022-08-23 18:51:26 -0300

    alteracoes

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-import userAction from '../redux/actions';
+import { userAction } from '../redux/actions';
 
 import '../Style/Login.css';
 import logo from '../images/logo1.png';
